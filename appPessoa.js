14a83db835956134cdf4b014826a92da1ebbc044
= <=>
2026-08-06 12:00:27 -0300

    atvtst

---
+++ appPessoa.js
@@ -0,0 +1,5 @@
+import Pessoa from './pessoa.js'
+
+let pessoa1 = new Pessoa("Fulano", "fulano@gmail.com", 99128340)
+
+console.log('N)
---
+++ pessoa.js
@@ -0,0 +1,9 @@
+export default class Pessoa {
+        constructor(nome, email, celular){
+            this.nome = nome
+            this.email = email
+            this.celular = celular
+        }
+
+    
+}
