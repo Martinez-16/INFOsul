f34675f851b1205fe34ca75468b63f262e5618da
Martinez-16 <josemartinez.bg024@academico.ifsul.edu.br>
2026-08-28 23:34:12 -0300

    atvatleta.js

--- 4-info/atv2/atvatleta/AppAtleta.js
+++ 4-info/atv2/atvatleta/AppAtleta.js
@@ -1,13 +1,20 @@
-import Atleta from "./atleta.js";
+import Atleta from './Atleta.js'
 import input from 'readline-sync'
 
 let nome = input.question("Informe seu nome de atleta: ")
 let idade = input.questionInt("Informe sua idade: ")
-let altura = input.questionFloat("Informe sua altura")
-let peso = input.questionFloat("Informe seu peso")
+let altura = input.questionFloat("Informe sua altura: ")
+let peso = input.questionFloat("Informe seu peso: ")
 let atleta = new Atleta (nome, idade, altura, peso)
 
 console.log("Esse App calcula se você ja pode se aposentar.")
 console.log("Calculando...")
-setTimeout(2)
-calculo
+
+if(atleta.okaposent(atleta.idade) == true){
+    console.log(`Parabéns, você já pode se aposentar!`)
+}
+else{
+    console.log(`Você ainda não pode se aposentar, faltam ${atleta.calculoAp(atleta.idade)} anos para você se aposentar.`)
+}
+
+
